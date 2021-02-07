345a32a17677593a856d0f53239ac113c3851787
Ben Agnew <48106124+bgnw@users.noreply.github.com>
2021-02-07 12:36:10 +0000

    Add error pages (IP/password restrictions)

---
+++ err/cred.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$home = $_SERVER['CONTEXT_DOCUMENT_ROOT'];
+require_once "$home/incl/head.php";
+?>
+<body>
+
+
+<div class="card error">
+  <p class="title">Access Denied</p>
+  <p>Sorry - you provided incorrect credentials for that page.</p>
+  <br>
+  <div class="link-collection">
+    <a class="link" href="javascript:history.back()"><p>Go Back &rsaquo;</p></a>
+    <a class="link" href="../"><p>Homepage &rsaquo;</p></a>
+  </div>
+</div>
+
+
+<?php include_once "$home/incl/footer.php"; ?>
+</body>
+</html>
